488492e573c47a2204d663dfda1037d492ef23f2
Mark Hunter <mark@Marks-MacBook-Pro-2.local>
2017-04-30 00:15:19 -0400

    more basics

--- simple_app.py
+++ simple_app.py
@@ -3,15 +3,43 @@ Purpose: gain familiarity
 
 '''
 from flask import Flask
-from flask import request #Flask global that represents the request that the client has made to your application. This contains things like cookies, the path, and, in our usage, the query string.
+#from flask import request #Flask global that represents the request that the client has made to your application. This contains things like cookies, the path, and, in our usage, the query string.
 
 app = Flask(__name__) #use whatever the current namespace is
 
 #the following is a decorator, a function that wraps around another function
 #the '/' indicates to route to this function on the websites index ie www.website.com/ <-
 @app.route('/')
-#function/view
+@app.route('/<name>') #views can have more than one route!, this tells flask to capture what comes after as a variable
+#function/view to display name that is passed in after url, defaults to 'Mark'
 def index(name='Mark'):
+
+    '''
+
+    query string: (ugly) The part of a URL that comes after the ?. You'll notice that the information after this looks like keyword arguments.
+    name = request.args.get('name', name) #if we get a name will use that, if we dont use the default name
+                                        #i.e. url/?name=Bill is how Bill will be passed into the function
+
+    '''
+
     return 'Hello World, I <3 ' + name
+
+#when passing arguments via the route url they are of type string unless otherwise specified
+#if something is a typed cased wrong we will get a 404 error
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+#function/view to return the sum of the two numbers (either floats or ints) entered
+def add(num1, num2):
+    sum = num1 + num2
+    return '{} + {} = {}'.format(num1, num2, sum) #we can only return stings
+
+
+@app.route('/multiply/<int:num1>/<int:num2>')
+#view to multiply two ints
+def multiply(num1, num2):
+    product = num1 * num2
+    return '{} x {} = {}'.format(num1,num2,product)
 
 app.run(debug=True, port=5000, host='127.0.0.1') #debug is true so that it updates when we make changes
